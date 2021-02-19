dfc55d48fbf1fa6ada798f0ead931cc84005fddf
boolfalse <boolfalse@mail.ru>
2021-02-20 00:34:05 +0400

    Components definitions and initial usage

---
+++ src/components/v-cart.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="v-cart"></div>
+</template>
+
+<script>
+export default {
+  name: "v-cart",
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {}
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/v-catalog.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="v-catalog">
+  <h2>Catalog</h2>
+  <v-catalog-item />
+  <v-catalog-item />
+  <v-catalog-item />
+</div>
+</template>
+
+<script>
+import vCatalogItem from './v-catalog-item'
+
+export default {
+  name: "v-catalog",
+  components: {
+    vCatalogItem
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {}
+}
+</script>
+
+<style>
+</style>
