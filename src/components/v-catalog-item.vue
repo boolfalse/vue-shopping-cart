6f893ca31b07170e718865c6b05156e59a38d9b6
boolfalse <boolfalse@mail.ru>
2021-03-03 19:09:59 +0400

    Data Binding: Parent to Child components

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="v-catalog-item">
-    <p>Item 1</p>
-    <p>Price: 100</p>
-    <button>Add to cart</button>
+    <img class="v-catalog-item__image" v-bind:src="require('./../assets/images/' + product_data.image)" alt="Image">
+    <p class="v-catalog-item__name">{{product_data.name}}</p>
+    <p class="v-catalog-item__price">Price: ${{product_data.price}}</p>
+    <button class="v-catalog-item__add_to_cart_btn btn">Add to cart</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "v-catalog-item",
-  props: {},
+  props: {
+    product_data: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
   data() {
     return {}
   },
@@ -23,5 +31,10 @@ export default {
   box-shadow: 0 0 8px #e0e0e0;
   padding: $padding*2;
   margin-bottom: $margin*2;
+
+  &__image {
+    max-width: 100px;
+    width: 50%;
+  }
 }
 </style>
