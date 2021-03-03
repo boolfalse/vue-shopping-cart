97aa8b7ab2501a638e82f082e0bedb7974761a2c
boolfalse <boolfalse@mail.ru>
2021-03-03 19:56:04 +0400

    Vuex initial definition

---
+++ src/vuex/store.js
@@ -0,0 +1,20 @@
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+let store = new Vuex.Store({
+    state: {}, // variables, objects, massives, collections, etc
+    getters: { // opportunity for getting the "state" data with shortcuts
+        //
+    },
+    mutations: { // synchronized tool for changing the "state" data
+        //
+    },
+    actions: { // async tool for changing the "state" data
+        //
+    }
+});
+
+export default store;
